ddadbde66ab9e7be8e84329cd58e9d8201043c8f
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2026-05-15 12:17:12 +0200

    [nrf noup] Don't include legacy mbedTLS headers when not needed
    The implementation of crypto_platform.cpp unconditionally included
    legacy mbedtls headers even when OPENTHREAD_CONFIG_CRYPTO_LIB_MBEDTLS
    is not enabled. Update that to only include the legacy headers
    when OPENTHREAD_CONFIG_CRYPTO_LIB_MBEDTLS as it should.
    
    This is a noup because crypto_platform.cpp is already been refactored
    to two separate files one for mbedtls and one for PSA in a later version
    so this fix cannot go upstream. This is needed now for the update
    to the TF-PSA-Crypto since this will remove all the mbedts legacy
    headers and thus any reference to them will failo build.
    
    When OT is updated to a later version where the file crypto_platform.cpp
    is split the current commit can be dropped.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- src/core/crypto/crypto_platform.cpp
+++ src/core/crypto/crypto_platform.cpp
@@ -34,15 +34,6 @@
 
 #include <string.h>
 
-#include <mbedtls/aes.h>
-#include <mbedtls/cmac.h>
-#include <mbedtls/ctr_drbg.h>
-#include <mbedtls/ecdsa.h>
-#include <mbedtls/entropy.h>
-#include <mbedtls/md.h>
-#include <mbedtls/pk.h>
-#include <mbedtls/sha256.h>
-#include <mbedtls/version.h>
 
 #include <openthread/instance.h>
 #include <openthread/platform/crypto.h>
@@ -62,6 +53,16 @@ using namespace ot;
 using namespace Crypto;
 
 #if OPENTHREAD_CONFIG_CRYPTO_LIB == OPENTHREAD_CONFIG_CRYPTO_LIB_MBEDTLS
+
+#include <mbedtls/aes.h>
+#include <mbedtls/cmac.h>
+#include <mbedtls/ctr_drbg.h>
+#include <mbedtls/ecdsa.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/md.h>
+#include <mbedtls/pk.h>
+#include <mbedtls/sha256.h>
+#include <mbedtls/version.h>
 
 //---------------------------------------------------------------------------------------------------------------------
 // Default/weak implementation of crypto platform APIs
